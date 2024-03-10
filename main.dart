25ed3ac1f1bcc539ba060e9bb6eda911866853fc
Anshika Verma <142977163+Verma-Ansh@users.noreply.github.com>
2024-03-10 13:27:42 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -12,6 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        title: 'Jia', debugShowCheckedModeBanner: false, home: HomePage());
+        title: 'Jarvis', debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
